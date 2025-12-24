0a9b8f8d15eeeb049e791a64db2b4d60fb6419ce
praveen <shankar.ankepaka@ftprotech.com>
2025-12-24 15:45:21 +0530

    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:opsentra_hr/main.dart';
 
 void main() {
+  // test
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
