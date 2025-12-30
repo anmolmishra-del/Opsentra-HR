8918abbd9a72d8af0c0fdef4f2ab0008cd907bfe
praveen <shankar.ankepaka@ftprotech.com>
2025-12-30 17:09:25 +0530

    localization

--- lib/Core/network/api_service.dart
+++ lib/Core/network/api_service.dart
@@ -1,22 +1,14 @@
-
 import 'package:opsentra_hr/Core/constants/api_constants.dart';
 import 'package:opsentra_hr/Core/network/api_client.dart';
-
-import '../../core/storage/secure_storage.dart';
+import 'package:opsentra_hr/Core/storage/secure_storage.dart';
 
 class AuthApi {
   final ApiClient _client = ApiClient();
 
-  Future<void> login({
-    required String email,
-    required String password,
-  }) async {
+  Future<void> login({required String email, required String password}) async {
     final response = await _client.post(
       ApiEndpoints.baseUrl + ApiEndpoints.login,
-      body: {
-        "email": email,
-        "password": password,
-      },
+      body: {"email": email, "password": password},
     );
 
     final token = response["access_token"];
